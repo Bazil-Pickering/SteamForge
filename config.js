e2321181b0f01b10323d8b9420d472da233d407f
Bazil <bazilregan@gmail.com>
2026-08-24 22:44:42 +0200

    Change TMDB_TOKEN in config.js
    Updated TMDB_TOKEN with a new access token.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // PASTE YOUR TMDB READ ACCESS TOKEN HERE
-    TMDB_TOKEN: "YOUR_TMDB_READ_ACCESS_TOKEN",
+    TMDB_TOKEN: "df59c593c98828920701169324da8a0e",
     VIDKING_COLOR: "8b5cf6",
     API_BASE: "https://api.themoviedb.org/3",
     IMG_BASE_W500: "https://image.tmdb.org/t/p/w500",
